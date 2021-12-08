669886956e008b1ec2eef757fc28a09439933c82
krimint <jumpy.3gp@gmail.com>
2021-12-09 00:52:50 +0700

    Axel - Shift Fix Bug V 2.0

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,13 +24,14 @@ class AuthController extends Controller
         if(Auth::attempt($credentials)){
             $request->session()->regenerate();
             // cek ke data sblmnya shift
-            $cek = Shift::latest()->where('user_id',auth()->user()->id)
-                    ->whereDate('waktu_awal',date('Y-m-d'))
+            $cek = Shift::where('user_id',auth()->user()->id)
+                    ->latest()
                     ->first();
-            if(Auth::user()->role == 'User'){
-                if(is_null($cek)){
-                    return redirect()->intended('cashawal');
-                }
+            if(Auth::user()->role == 'User' && $cek != null){
+                session([
+                    'shift' => 'N'
+                ]);
+                return redirect()->intended('cashawal');
             }
             /////
             return redirect()->intended('dashboard');
@@ -39,18 +40,13 @@ class AuthController extends Controller
         return back()->with('loginErr','Login gagal!');
     }
 
-    // public function tes(){
-    //     $user = User::find(Auth::user()->id);
-    //     return $user->shift;
-    // }
-
     public function logout(Request $request)
     {
-        $cek = Shift::latest()->where('user_id',auth()->user()->id)->first();
-        if(Auth::user()->role == 'User'){
-            if(empty($cek->cash_akhir)){
-                return view('shift.cashakhir',compact('cek'));
-            }
+        $cek = Shift::where('user_id',auth()->user()->id)
+                    ->latest()
+                    ->first();
+        if(Auth::user()->role == 'User' && session()->get('shift') == 'Y'){
+            return view('shift.cashakhir',compact('cek'));
         }
 
         Auth::logout();
