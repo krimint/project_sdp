2f1bf87dd07d03a594828cf8fbee33d43df3dcb5
krimint <jumpy.3gp@gmail.com>
2021-12-09 01:49:47 +0700

    Axel - Pegawai V3

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -31,7 +31,7 @@
             <!-- Sidebar user (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                <img src="/assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img style="width:50px; height:50px;" src="{{ asset("storage/image/pengguna/".Auth::user()->email) }}.jpg"  class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                 <a href="#" class="d-block">{{ Auth::user()->name }}</a>
