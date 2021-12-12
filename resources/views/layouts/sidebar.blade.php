12e026531ca74d2e4556749feb318959e82df6c7
krimint <jumpy.3gp@gmail.com>
2021-12-12 11:36:26 +0700

    Axel - Perbaikan Sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -22,7 +22,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="/assets/index3.html" class="brand-link">
+    <a href="#" class="brand-link">
         <img src="/assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Resto</span>
     </a>
@@ -31,7 +31,7 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-            <img style="width:50px; height:50px;" src="/<?= (Auth::user()->image != null) ? 'image/'.Auth::user()->image.'' : 'assets/dist/img/user2-160x160.jpg'?>"  class="img-circle elevation-2" alt="User Image">
+            <img style="width:50px; height:50px;" src="{{ asset("storage/image/pengguna/".Auth::user()->email) }}.jpg"  class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
             <a href="#" class="d-block">{{ Auth::user()->name }}</a>
